8a84813a7052b665fe87a61c722c0dda7a7e9b6f
Marko Mäkelä <marko.makela@mariadb.com>
2021-08-02 16:07:43 +0300

    Introduce a simple variant of dict_table_close()

--- storage/innobase/row/row0mysql.cc
+++ storage/innobase/row/row0mysql.cc
@@ -966,7 +966,7 @@ void row_prebuilt_free(row_prebuilt_t *prebuilt)
 		rtr_clean_rtr_info(prebuilt->rtr_info, true);
 	}
 	if (prebuilt->table) {
-		dict_table_close(prebuilt->table, false, false);
+		dict_table_close(prebuilt->table);
 	}
 
 	mem_heap_free(prebuilt->heap);
